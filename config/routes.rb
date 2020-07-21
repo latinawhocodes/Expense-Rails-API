070b2530119dce7e4cb64058a7846072d64b7c89
StephanieAlyssa <alyssa.yesilyurt@gmail.com>
2020-07-20 23:50:18 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do 
     resources :expenses do 
-      resources :entries
+    resources :entries
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
